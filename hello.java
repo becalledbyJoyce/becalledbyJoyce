0877b2b7e09dbec7f6451a8e2aed8ee835a7b5c6
JEFF <88610589+becalledbyJoyce@users.noreply.github.com>
2021-09-26 00:27:07 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,9 @@
-
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }   
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
